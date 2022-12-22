8d6b5fa229ff425937ff8ecdb16e0056c80ab0d3
Luis Ribeiro <luis.mribeiro@sapo.pt>
2022-12-22 18:35:07 +0000

    Use timestamps in files

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ else
 	echo $'\n' "------ CONFIG SUCCESSFUL! ---------------------" $'\n'
 fi
 
+echo $'\n' "------ SYNC START -------------------------" $'\n'
+
 rsync --progress -avzh \
 	--exclude='.git/' \
 	--exclude='.git*' \
@@ -29,6 +31,27 @@ rsync --progress -avzh \
 if [ $? -eq 0 ]
 then
 	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
+	echo $'\n' "------ GENERATING VERSION FILES ---------------" $'\n'
+
+  echo $'' "- Delete old files " $''
+  rm js/app_*
+  rm js/index_*
+
+  echo $'' "- Generate new files " $''
+  timestamp=$(date +%s)
+  cp js/app.js js/app_$timestamp.js
+  cp js/index.js js/index_$timestamp.js
+
+  echo $'' "- Replace imports " $''
+  app=app_$timestamp.js
+  index=index_$timestamp.js
+
+  sed -i.back 's/js\/app.js/js\/'$app'/g' 'index.html'
+  sed -i.back 's/js\/index.js/js\/'$index'/g' 'index.html'
+
+  echo $'' "- Delete back file " $''
+  rm index.html.back
+
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
 
 	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chown -R $4:$4 $3"
