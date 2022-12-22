19596e6fc212838e5108c83ce7fc07eeb331680f
Luis Ribeiro <luis.mribeiro@sapo.pt>
2022-12-22 20:52:15 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ then
 	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
 	echo $'\n' "------ GENERATING VERSION FILES ---------------" $'\n'
 
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sh script.sh"
+	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sh $3/script.sh"
 
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
 
